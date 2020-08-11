f3daa99f0973fc05c57000f0c37bd58bbd2b5736
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-08-11 08:39:07 +0200

    Init feed export refactor

---
+++ src/Model/Export/ArticleCollection.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\FactFinder\Oxid\Model\Export;
+
+use OxidEsales\Eshop\Application\Model\Article;
+use OxidEsales\Eshop\Application\Model\ArticleList;
+
+class ArticleCollection implements \IteratorAggregate
+{
+    /** @var int */
+    private $batchSize;
+
+    public function __construct(int $batchSize = 100)
+    {
+        $this->batchSize = $batchSize;
+    }
+
+    /**
+     * @return Article[]
+     */
+    public function getIterator(): iterable
+    {
+        yield from []; // Init iterator
+
+        $from  = 0;
+        $batch = $this->getBatch($from);
+        while ($batch->count()) {
+            yield from $batch;
+            $batch = $this->getBatch(++$from);
+        }
+    }
+
+    protected function getBatch(int $from): ArticleList
+    {
+        $articleList = oxNew(ArticleList::class);
+        $articleList->setSqlLimit($from * $this->batchSize, $this->batchSize);
+        return $articleList->getList();
+    }
+}
