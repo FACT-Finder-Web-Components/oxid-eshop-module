6730a635d0dc16e9112afc7aab27dd2db6c6c60f
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-09-24 21:23:56 +0200

    Deprecate Serializer

--- src/Model/Api/Client.php
+++ src/Model/Api/Client.php
@@ -9,19 +9,10 @@ use Guzzle\Http\Exception\ServerErrorResponseException;
 use Guzzle\Http\Message\RequestInterface;
 use Guzzle\Http\Message\Response;
 use Omikron\FactFinder\Oxid\Contract\Api\ClientInterface;
-use Omikron\FactFinder\Oxid\Contract\Api\SerializerInterface;
 use Omikron\FactFinder\Oxid\Exception\ResponseException;
 
 class Client implements ClientInterface
 {
-    /** @var SerializerInterface */
-    private $serializer;
-
-    public function __construct(SerializerInterface $serializer)
-    {
-        $this->serializer = $serializer;
-    }
-
     protected function client(): HttpClient
     {
         return new HttpClient();
@@ -53,7 +44,7 @@ class Client implements ClientInterface
             $request->setHeaders(['Accept' => 'application/json'] + $headers);
             $response = $request->send();
             if ($response->isSuccessful()) {
-                return $this->serializer->unserialize((string) $response->getBody());
+                return (array) $response->json();
             }
 
             $this->badRequest($response);
@@ -66,7 +57,7 @@ class Client implements ClientInterface
 
     protected function badRequest(Response $response)
     {
-        $errorMessage = current($this->serializer->unserialize((string) $response->getBody()));
+        $errorMessage = current((array) $response->json());
         throw new ResponseException($errorMessage['error'] ?? $errorMessage, $response->getStatusCode());
     }
 }
--- src/Model/Api/ClientFactory.php
+++ src/Model/Api/ClientFactory.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Oxid\Model\Api;
 
 use Omikron\FactFinder\Oxid\Contract\Api\ClientInterface;
-use Omikron\FactFinder\Oxid\Model\Api\Serializer\Json;
 
 class ClientFactory
 {
     public function create(): ClientInterface
     {
-        return new Client(new Json());
+        return new Client();
     }
 }
