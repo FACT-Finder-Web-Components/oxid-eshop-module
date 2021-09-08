816bebf5beac47c1236ba6a856c74c262c6e358e
Pavel Novikov <paulfcdd@gmail.com>
2021-09-08 14:17:01 +0200

    FFWEB-2168: ArticleFeed.php extends AbstractFeed, small changes

--- src/Export/ArticleFeed.php
+++ src/Export/ArticleFeed.php
@@ -7,16 +7,15 @@ namespace Omikron\FactFinder\Oxid\Export;
 use Omikron\FactFinder\Oxid\Export\Data\ArticleCollection;
 use Omikron\FactFinder\Oxid\Export\Entity\DataProvider;
 use Omikron\FactFinder\Oxid\Export\Field\Attribute as AttributeField;
+use Omikron\FactFinder\Oxid\Export\Field\BaseFieldInterface as FieldInterface;
 use Omikron\FactFinder\Oxid\Export\Field\Brand;
 use Omikron\FactFinder\Oxid\Export\Field\CategoryPath;
-use Omikron\FactFinder\Oxid\Export\Field\FieldInterface;
 use Omikron\FactFinder\Oxid\Export\Field\FilterAttributes;
 use Omikron\FactFinder\Oxid\Export\Field\Keywords;
 use Omikron\FactFinder\Oxid\Export\Stream\StreamInterface;
 use Omikron\FactFinder\Oxid\Model\Config\Export as ExportConfig;
-use OxidEsales\Eshop\Core\Registry;
 
-class ArticleFeed
+class ArticleFeed extends AbstractFeed
 {
     /** @var FieldInterface[] */
     protected $fields;
@@ -38,27 +37,18 @@ class ArticleFeed
         $this->fields = $fields;
     }
 
-    public function getFileName(): string
+    protected function getFieldName(FieldInterface $field): string
     {
-        return sprintf('export.%s.csv', $this->getChannel(Registry::getLang()->getLanguageAbbr()));
+        return parent::getFieldName($field);
     }
 
     public function generate(StreamInterface $stream): void
     {
         $fields  = array_merge($this->getAdditionalFields(), $this->getConfigFields(), $this->fields);
         $columns = array_unique(array_merge($this->columns, array_map([$this, 'getFieldName'], $fields)));
+
         $stream->addEntity($columns);
         oxNew(Exporter::class)->exportEntities($stream, oxNew(DataProvider::class, ...$fields), $columns, oxNew(ArticleCollection::class));
-    }
-
-    protected function getChannel(string $lang): string
-    {
-        return Registry::getConfig()->getConfigParam('ffChannel')[$lang];
-    }
-
-    protected function getFieldName(FieldInterface $field): string
-    {
-        return $field->getName();
     }
 
     protected function getAdditionalFields(): array
