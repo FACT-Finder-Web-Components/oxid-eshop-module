c171fa7eb4dab5d605a5bf7ea2955b40d3cf59a8
Pavel Novikov <paulfcdd@gmail.com>
2022-02-07 16:17:38 +0100

    FFWEB-2304: apply cs-fix & refactoring

--- src/Model/Config/Communication.php
+++ src/Model/Config/Communication.php
@@ -128,12 +128,12 @@ class Communication implements ParametersSourceInterface
         $path              = 'ROOT';
         $value             = ['navigation=true'];
         foreach ($categoriesReverse as $key => $category) {
-            if ($key === 0) {
-                $value[] = sprintf("filter{$param}%s=%s", $path, $this->urlPlusEncodeCategoryPath($category));
-            } else {
+            $filterValue = sprintf("filter{$param}%s=%s", $path, $this->urlPlusEncodeCategoryPath($category));
+            if ($key >= 1) {
                 $path .= $this->urlPlusEncodeCategoryPath(urlencode('/') . $categoriesReverse[$key - 1]);
-                $value[] = sprintf("filter{$param}%s=%s", $path, $this->urlPlusEncodeCategoryPath($category));
+                $filterValue = sprintf("filter{$param}%s=%s", $path, $this->urlPlusEncodeCategoryPath($category));
             }
+            $value[] = $filterValue;
         }
 
         return implode(',', $value);
