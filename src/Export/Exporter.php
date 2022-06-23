9666032916769227d57cd6ccdb946393c9a4a5a9
gjamroz <gjamroz@objectivity.co.uk>
2022-06-23 11:44:34 +0200

    FFWEB-2402: added log path option and proceed with export while error option

--- src/Export/Exporter.php
+++ src/Export/Exporter.php
@@ -5,17 +5,30 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Oxid\Export;
 
 use Omikron\FactFinder\Oxid\Export\Entity\DataProviderInterface;
+use Omikron\FactFinder\Oxid\Export\Entity\ExportEntityInterface;
 use Omikron\FactFinder\Oxid\Export\Filter\TextFilter;
 use Omikron\FactFinder\Oxid\Export\Stream\StreamInterface;
+use Omikron\FactFinder\Oxid\Utilities\FfLogger;
+use OxidEsales\Eshop\Core\Registry;
 
 class Exporter implements ExporterInterface
 {
     /** @var TextFilter */
     private $filter;
 
+    /** @var bool */
+    private $proceedWhileError;
+
+    /**
+     * @var FfLogger
+     */
+    private $logger;
+
     public function __construct()
     {
         $this->filter = oxNew(TextFilter::class);
+        $this->proceedWhileError = boolval(Registry::getConfig()->getConfigParam('ffIsProceedWhileError'));
+        $this->logger = new FfLogger('exporter');
     }
 
     public function exportEntities(StreamInterface $stream, DataProviderInterface $dataProvider, array $columns): void
@@ -23,9 +36,32 @@ class Exporter implements ExporterInterface
         $emptyRecord = array_combine($columns, array_fill(0, count($columns), ''));
 
         foreach ($dataProvider->getEntities() as $entity) {
-            $entityData = array_merge($emptyRecord, array_intersect_key($entity->toArray(), $emptyRecord)); // phpcs:ignore
-            $stream->addEntity($this->prepare($entityData));
+            try {
+                $entityData = array_merge($emptyRecord, array_intersect_key($entity->toArray(), $emptyRecord)); // phpcs:ignore
+                $stream->addEntity($this->prepare($entityData));
+            } catch (\Throwable $e) {
+                $this->handleError($e, $entity);
+            }
         }
+    }
+
+    private function handleError(\Throwable $e, ExportEntityInterface $entity)
+    {
+        if ($this->proceedWhileError === false) {
+            throw $e;
+        }
+
+        $entity = array_filter($entity->toArray(), function($v) {
+            return in_array($v, [
+                'ProductNumber',
+                'Name',
+                'Id',
+                'ShopId',
+                'ParentId',
+                'RootId',
+            ]);
+        }, ARRAY_FILTER_USE_KEY);
+        $this->logger->error($e->getMessage(), ['entity' => $entity]);
     }
 
     private function prepare(array $data): array
---
+++ src/Utilities/FfLogger.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Omikron\FactFinder\Oxid\Utilities;
+
+use DateTimeZone;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use OxidEsales\Eshop\Core\Registry;
+
+class FfLogger extends Logger
+{
+    public function __construct(string $name, array $handlers = [], array $processors = [], ?DateTimeZone $timezone = null)
+    {
+        parent::__construct($name, $handlers, $processors, $timezone);
+
+        $logPath = strval(Registry::getConfig()->getConfigParam('ffLogPath'));
+        $logPath = $logPath === '' ? sprintf('%s/log', OX_BASE_PATH) : $logPath;
+        $this->pushHandler(new StreamHandler(sprintf('%s/fact-finder/%s.log', $logPath, $name)));
+    }
+}
