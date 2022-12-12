c8529b0a0a30ff56ee401ac27c0a9f7be7cb8774
Grzegorz Jamróz <grzegorz.jamrooz@gmail.com>
2022-12-12 08:39:44 +0100

    FFWEB-2639 - cart tracking fix

--- src/out/js/tracking.js
+++ src/out/js/tracking.js
@@ -1,32 +1,41 @@
 function registerAddToCartListener(selector, productId) {
-    document.addEventListener('WebComponentsReady', function () {
-        const trackingHelper = factfinder.communication.Util.trackingHelper;
+    let trackingSent = false;
+    const trackingHelper = factfinder.communication.Util.trackingHelper;
 
-        function trackAddToCart(product) {
-            factfinder.communication.trackingManager.cart({
-                id: trackingHelper.getTrackingProductId(product),
-                masterId: trackingHelper.getMasterArticleNumber(product),
-                price: trackingHelper.getPrice(product),
-                title: trackingHelper.getTitle(product),
-                count: 1,
-            });
-        }
+    function trackAddToCart(product) {
+        factfinder.communication.Tracking.cart({
+            id: trackingHelper.getTrackingProductId(product),
+            masterId: trackingHelper.getMasterArticleNumber(product),
+            price: trackingHelper.getPrice(product),
+            title: trackingHelper.getTitle(product),
+            count: 1,
+        });
+    }
 
-        const element = document.querySelector(selector);
-        if (element) {
-            element.addEventListener('click', function () {
-                factfinder.communication.EventAggregator.addFFEvent({
-                    type: 'getRecords',
-                    recordId: productId,
-                    idType: 'productNumber',
-                    success: function (response) {
-                        if (response && response[0]) {
-                            const product = response[0];
-                            trackAddToCart(product);
-                        }
-                    },
-                });
+    const element = document.querySelector(selector);
+
+    if (element) {
+        element.addEventListener('submit', function (e) {
+            if (trackingSent === false) {
+                e.preventDefault();
+            }
+
+            factfinder.communication.EventAggregator.addFFEvent({
+                type: 'getRecords',
+                recordId: productId,
+                idType: 'productNumber',
+                success: function (response) {
+                    if (response && response[0]) {
+                        const product = response[0];
+                        trackAddToCart(product);
+                    }
+
+                    if (trackingSent === false) {
+                        trackingSent = true;
+                        e.target.submit();
+                    }
+                },
             });
-        }
-    });
+        });
+    }
 }
