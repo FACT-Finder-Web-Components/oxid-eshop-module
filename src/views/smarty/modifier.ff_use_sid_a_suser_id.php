ff8bb2bce81ecda388478b78d591ef3848d3631b
gjamroz <gjamroz@objectivity.co.uk>
2023-02-28 10:40:47 +0100

    FFWEB-2674: Add a switch to send the SID as userId when user not logged in

--- src/views/smarty/modifier.ff_use_sid_a_suser_id.php
+++ src/views/smarty/modifier.ff_use_sid_a_suser_id.php
@@ -6,6 +6,9 @@ use OxidEsales\Eshop\Core\Config;
 use OxidEsales\Eshop\Core\Model\BaseModel;
 use OxidEsales\Eshop\Core\Registry;
 
+/**
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
 function smarty_modifier_ff_use_sid_a_suser_id(BaseModel $article): bool
 {
     /** @var Config $config */
