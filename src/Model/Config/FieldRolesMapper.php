654f71ef2d0945cd6f0ea8661f1e7099b8d2b844
Robert Saternus <robert.saternus@gmail.com>
2024-12-03 13:58:46 +0100

    FFWEB-3245: Field roles and formatting
    Field roles and formatting

--- src/Model/Config/FieldRolesMapper.php
+++ src/Model/Config/FieldRolesMapper.php
@@ -12,16 +12,15 @@ class FieldRolesMapper
 
         return [
             'brand'                 => $getRole('brand'),
-            'campaignProductNumber' => $getRole('productNumber'),
             'deeplink'              => $getRole('deeplink'),
             'description'           => $getRole('description'),
             'displayProductNumber'  => $getRole('productNumber'),
             'ean'                   => $getRole('ean'),
             'imageUrl'              => $getRole('imageUrl'),
-            'masterArticleNumber'   => $getRole('masterId'),
+            'masterId'              => $getRole('masterId'),
             'price'                 => $getRole('price'),
             'productName'           => $getRole('productName'),
-            'trackingProductNumber' => $getRole('productNumber'),
+            'productNumber'         => $getRole('productNumber'),
         ];
     }
 
