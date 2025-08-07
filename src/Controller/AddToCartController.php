ebd9a89a82b4619fdd4c200d8815d3aef7b3b05e
robertsaternus <robert.saternus@gmail.com>
2025-08-07 14:26:34 +0200

    Fix CI

--- src/Controller/AddToCartController.php
+++ src/Controller/AddToCartController.php
@@ -5,28 +5,27 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Oxid\Controller;
 
 use OxidEsales\Eshop\Application\Controller\FrontendController;
-use OxidEsales\Eshop\Core\Registry;
-use OxidEsales\Eshop\Application\Model\Basket;
 use OxidEsales\Eshop\Application\Model\Article;
+use OxidEsales\Eshop\Core\Registry;
 
 class AddToCartController extends FrontendController
 {
     public function addToCart(): void
     {
         $productNumber = Registry::getRequest()->getRequestParameter('productNumber');
-        $amount = 1;
-        $utilsView = Registry::getUtilsView();
+        $amount        = 1;
+        $utilsView     = Registry::getUtilsView();
 
         try {
             if (!$productNumber) {
-                throw new \Exception('Product with number ' . $productNumber . ' does not exist');
+                throw new \Exception("Product with number $productNumber does not exist");
             }
 
-            $product = oxNew(Article::class);
+            $product   = oxNew(Article::class);
             $productId = $this->getProductIdByNumber($productNumber);
 
             if (!$productId || !$product->load($productId) || !$product->isBuyable()) {
-                throw new \Exception('Product with number ' . $productNumber . ' does not exist or is not buyable');
+                throw new \Exception("Product with number $productNumber does not exist or is not buyable");
             }
 
             $basket = Registry::getSession()->getBasket();
@@ -42,10 +41,10 @@ class AddToCartController extends FrontendController
 
     private function getProductIdByNumber($productNumber)
     {
-        $db = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
-        $sQuery = "SELECT oxid FROM oxarticles WHERE oxartnum = ? AND oxactive = 1";
+        $database     = \OxidEsales\Eshop\Core\DatabaseProvider::getDb();
+        $sQuery       = 'SELECT oxid FROM oxarticles WHERE oxartnum = ? AND oxactive = 1';
 
-        return $db->getOne($sQuery, [$productNumber]);
+        return $database->getOne($sQuery, [$productNumber]);
     }
 
     private function redirectToReferer(): void
